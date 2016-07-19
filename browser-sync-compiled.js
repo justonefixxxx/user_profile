407f13eeb1203001dee6ae3cb9fd99abb95f4c2a
justonefixxxx <justonefixxxx@gmail.com>
2016-07-19 23:35:18 +0300

    removed untracked files

--- browser-sync-compiled.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var browserSync = require('browser-sync');
-module.exports = browserSync({
-	proxy: 'localhost:3000',
-	files: ['app/**/*.*']
-});
-
-//# sourceMappingURL=browser-sync-compiled.js.map
--- browser-sync.js
+++
@@ -1,5 +0,0 @@
-var browserSync = require('browser-sync');
-module.exports = browserSync({
-	proxy: 'localhost:3000',
-	files: ['app/**/*.*']
-});
